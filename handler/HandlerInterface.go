b5a06f6c3fce2e036fbcee72b0a0e14be8bb78a4
ゴ・ヴァン・ギ <ngo.van.nghi@aainc.co.jp>
2017-06-28 19:59:04 +0900

    Add Logger Interface

--- handler/HandlerInterface.go
+++ handler/HandlerInterface.go
@@ -1,1 +1,4 @@
 package handler
+
+type HandlerInterface interface {
+}
--- logger.go
+++ logger.go
@@ -1,2 +1,27 @@
 package VyLog
 
+const (
+	TRACE = iota
+	DEBUG
+	INFO
+	WARN
+	ERROR
+	FATAL
+)
+
+type LoggerInterface interface {
+
+	Trace(string, ...interface{})
+
+	Debug(string, ...interface{})
+
+	Info(string, ...interface{})
+
+	Warn(string, ...interface{})
+
+	Error(string, ...interface{})
+
+	Fatal(string, ...interface{})
+
+	Log(int, string, ...interface{})
+}
