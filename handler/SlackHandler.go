ab06b00812408ef8eeddde065fb8c65af75cb51a
ゴ・ヴァン・ギ <ngo.van.nghi@aainc.co.jp>
2017-07-04 20:04:32 +0900

    Update slack handler

--- handler/SlackHandler.go
+++ handler/SlackHandler.go
@@ -2,19 +2,53 @@ package handler
 
 import (
 	"github.com/ngonghi/VyLog/common"
+	"github.com/ngonghi/VyLog"
+	"encoding/json"
+)
+
+const (
+	SLACK_COLOR_DANGER = "danger"
+	SLACK_COLOR_WARNING = "warning"
+	SLACK_COLOR_GOOD = "good"
+	SLACK_COLOR_DEFAULT = "#e3e4e6"
 )
 
 type SlackHandler struct {
 	AbstractHandler
+
+	channel string
+	hookUrl string
+	username string
+	preText string
+}
+
+func (handler *SlackHandler) getColor(level int) string {
+	switch level {
+	case VyLog.INFO:
+		return SLACK_COLOR_GOOD
+	case VyLog.WARN:
+		return SLACK_COLOR_WARNING
+	case VyLog.ERROR, VyLog.FATAL:
+		return SLACK_COLOR_DANGER
+	default:
+		return SLACK_COLOR_DEFAULT
+	}
 }
 
 func (handler *SlackHandler) Handle(message *common.Message) {
 	if handler.IsHandling(message) {
-		handler.Write(message)
+		handler.Post(message)
 	}
 }
 
-func (handler * SlackHandler) Write(msg *common.Message) {
+func (handler * SlackHandler) Post(msg *common.Message) {
 	buf := []byte{}
 	buf = append(buf, handler.GetFormatter().Format(msg)...)
 }
+
+//func (handler * SlackHandler) createSlackMsgContent(msg *common.Message) string {
+//
+//
+//
+//
+//}
